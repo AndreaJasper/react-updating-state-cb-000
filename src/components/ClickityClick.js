fd7e88696078f8b09822cc107646978189ddb7ee
Andrea Jasper <andrea.nicole.jasper@gmail.com>
2021-04-26 13:56:22 +0000

    Automatically backed up by Learn

--- src/components/ClickityClick.js
+++ src/components/ClickityClick.js
@@ -1,1 +1,20 @@
-// Code ClickityClick Component Here
+import React from 'react'
+
+  class ClickityClick extends React.Component {
+    
+    constructor() {
+      super();
+      
+      this.state = {
+        hasBeenClicked: false
+      }
+    }
+    
+    render() {
+      return(
+        
+      );
+    }
+  }
+
+export default ClickityClick;
