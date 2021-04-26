667b93c4665909e65d0523b2e22633a26366beb0
Andrea Jasper <andrea.nicole.jasper@gmail.com>
2021-04-26 13:58:01 +0000

    Automatically backed up by Learn

--- src/components/ClickityClick.js
+++ src/components/ClickityClick.js
@@ -7,12 +7,19 @@ import React from 'react'
       
       this.state = {
         hasBeenClicked: false
-      }
+      };
+    }
+    
+    handlClick = () => {
+      
     }
     
     render() {
       return(
-        
+        <div>
+          <p>I have {this.state.hasBeenClicked ? null : 'not'} been clicked;</p>
+          <button onClick={this.handlClick}>Click me!</button>
+        </div>
       );
     }
   }
